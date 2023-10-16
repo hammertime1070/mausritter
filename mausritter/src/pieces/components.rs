1d89e2ba9b8abeb554f93806a2f2dd317782ca8d
hammertime1070 <mbkeith614@gmail.com>
2023-10-15 23:56:17 -0400

    Add Action Queue

--- mausritter/src/pieces/components.rs
+++ mausritter/src/pieces/components.rs
@@ -1,6 +1,11 @@
 use bevy::prelude::*;
 
+use crate::actions::Action;
+
 #[derive(Component)]
 pub struct Piece {
     pub kind: String
 }
+
+#[derive(Component, Default)]
+pub struct Actor(pub Option<Box<dyn Action>>)
